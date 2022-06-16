9f653e4d5b0cd0b2e2c67f732711234ae95af97c
tayyoakinrinsoye <tayoakinrinsoye@hotmail.com>
2022-06-16 06:10:16 +0000

    Adding all

---
+++ shell.h
@@ -0,0 +1,23 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+
+#define SIZE_MAX 1024
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+void prompt(void);
+char *_read(void);
+char **parser(char *str, int count);
+
+
+/* string handlers */
+char *_strcat(char *dest, char *src);
+int _strcmp(char *s1, char *s2);
+int word_count(char *str);
+
+#endif
